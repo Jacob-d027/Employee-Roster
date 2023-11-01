f824a20ba91d65ce0278e98aca4ffec4d0172db1
Jacob Davis <jacobdofficial@gmail.com>
2023-10-31 22:03:26 -0600

    Added second Table View controller for employee type and set up selection of employee type and check mark to indicate selection

--- EmployeeRoster/EmployeeTypeTableViewController.swift
+++ EmployeeRoster/EmployeeTypeTableViewController.swift
@@ -37,46 +37,27 @@ class EmployeeTypeTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "EmployeeTypeCell", for: indexPath)
 
         // Configure the cell...
-
+        let type = EmployeeType.allCases[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = type.description
+        cell.contentConfiguration = content
+        
+        if employeeType == type {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        employeeType = EmployeeType.allCases[indexPath.row]
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -84,6 +65,5 @@ class EmployeeTypeTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
